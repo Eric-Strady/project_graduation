cc96b673b7a3da95e83237c56c544dfd71f8b68c
Eric-Strady <strady60@gmail.com>
2019-04-11 16:43:06 +0200

    create about form type

---
+++ src/Form/AboutType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\About;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AboutType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description')
+            ->add('how_join_us')
+            ->add('created_at')
+            ->add('amap_gps_lat')
+            ->add('amap_gps_lng')
+            ->add('nb_members')
+            ->add('annual_membership_fee')
+            ->add('facebook_link')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => About::class,
+        ]);
+    }
+}
