209d37e6cbcbfc42a8c2029d306e9048a0761983
Eric-Strady <strady60@gmail.com>
2019-04-27 16:21:56 +0200

    create js file to display OpenStreetMap

---
+++ public/js/Map.js
@@ -0,0 +1,33 @@
+class Map {
+	constructor(container, lat, lng) {
+		this.container = container;
+		this.lat = lat;
+		this.lng = lng;
+		this.map;
+		this.generateMap();
+	}
+
+	generateMap() {
+		this.map = L.map(document.getElementById(this.container)).setView([this.lat, this.lng], 13);
+		this.getLayer();
+		this.generateMarkers();
+		return this.map;
+	}
+
+	getLayer() {
+		L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+		    attribution: 'donn&eacute;es &copy; <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+		    maxZoom: 18,
+		}).addTo(this.map);
+	}
+
+	generateMarkers() {
+		let marker = L.marker([this.lat, this.lng])
+			.addTo(this.map)
+			/*.on('click', generateForm)*/;
+	}
+}
+
+$(function() {
+	const map = new Map('map', latitude, longitude);
+});
