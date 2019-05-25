023a53cb780cf9efcc63a408834d5116ed160975
Eric-Strady <strady60@gmail.com>
2019-05-25 16:18:31 +0200

    add autosave plugin

--- public/js/adminPage/Tinymce.js
+++ public/js/adminPage/Tinymce.js
@@ -9,7 +9,7 @@ class Tinymce {
 			selector:this.selector,
     		height: 300,
     		language: 'fr_FR',
-			plugins: 'preview autolink link pagebreak nonbreaking advlist lists textcolor wordcount contextmenu colorpicker help',
+			plugins: 'autosave preview autolink link pagebreak nonbreaking advlist lists textcolor wordcount contextmenu colorpicker help',
 			block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6',
 			toolbar: 'formatselect | bold italic strikethrough forecolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat'
 		});
