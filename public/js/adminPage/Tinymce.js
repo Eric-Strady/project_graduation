5839d701e543271bba8b911a71f4485e32737847
Eric-Strady <strady60@gmail.com>
2019-05-25 14:49:01 +0200

    install Tinymce

---
+++ public/js/adminPage/Tinymce.js
@@ -0,0 +1,21 @@
+class Tinymce {
+	constructor(selector) {
+		this.selector = selector;
+		this.displayTextEditor();
+	}
+
+	displayTextEditor(){
+		tinymce.init({
+			selector:this.selector,
+    		height: 300,
+    		language: 'fr_FR',
+			plugins: 'preview autolink link pagebreak nonbreaking advlist lists textcolor wordcount contextmenu colorpicker help',
+			block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6',
+			toolbar: 'formatselect | bold italic strikethrough forecolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat'
+		});
+	}
+}
+
+$(function() {
+	const tinymce = new Tinymce(selector);
+});
