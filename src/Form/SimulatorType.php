bcf0291b61ebbc0fea6294a9d978b830d2cd3924
Eric-Strady <strady60@gmail.com>
2019-06-09 19:12:25 +0200

    create Simulator form

---
+++ src/Form/SimulatorType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contract;
+use App\Entity\FoodType;
+use App\Simulator\Simulator;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SimulatorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nb_adult', NumberType::class, [
+                'required' => true
+            ])
+            ->add('nb_child', NumberType::class, [
+                'required' => true
+            ])
+            ->add('total_price', NumberType::class, [
+                'required' => true
+            ])
+            ->add('food_type', EntityType::class, [
+                'class' => FoodType::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'expanded' => false,
+                'empty_data' => false
+            ])
+            ->add('contract', EntityType::class, [
+                'class' => Contract::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'empty_data' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Simulator::class,
+        ]);
+    }
+}
