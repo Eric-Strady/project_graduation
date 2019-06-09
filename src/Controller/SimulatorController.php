e40ca9f450f38a7c5107e65e4fae832c1a93bc71
Eric-Strady <strady60@gmail.com>
2019-06-09 19:12:43 +0200

    add simulator form

--- src/Controller/SimulatorController.php
+++ src/Controller/SimulatorController.php
@@ -4,16 +4,29 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use App\Form\SimulatorType;
+use App\Simulator\Simulator;
 
 class SimulatorController extends AbstractController
 {
     /**
      * @Route("/simulateur", name="simulator")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('front/simulator.html.twig');
+    	$simulator = new Simulator();
+    	$form = $this->createForm(SimulatorType::class, $simulator);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            return $this->redirectToRoute('contact');
+        }
+        return $this->render('front/simulator.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
