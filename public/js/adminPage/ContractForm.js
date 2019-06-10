1c5eaf99ef7cce535b27aff7652000bdb28123e8
Eric-Strady <strady60@gmail.com>
2019-06-10 16:07:35 +0200

    worked on products view in contracts form

--- public/js/adminPage/ContractForm.js
+++ public/js/adminPage/ContractForm.js
@@ -2,6 +2,9 @@ class ContractForm {
 	constructor(domElt) {
 		this.productContainer = domElt.productContainer;
 		this.productAddElt = domElt.productAddElt;
+		this.tableElt = domElt.tableElt;
+		this.tableBodyElt = domElt.tableBodyElt;
+		this.tableBodyRowElt = domElt.tableBodyRowElt;
 		this.selectElt = domElt.selectElt;
 		this.handleEmbeddedProductForm();
 	}
@@ -9,13 +12,17 @@ class ContractForm {
 	handleEmbeddedProductForm() {
 		this.productPrototype = $(this.productContainer).attr('data-prototype');
 		this.productIndex = $(this.productContainer).find('fieldset').length;
-		console.log(this.productIndex);
 		let self = this;
 
 		if (this.productIndex !== 0) {
-			$(this.productContainer).children('fieldset').each(function() {
-				self.addDeleteLink($(this));
+			$(this.productContainer).children('fieldset').each(function(i) {
+				self.addTableLine(i, $(this));
+				i++;
 			});
+		}
+		else
+		{
+			$(this.tableElt).hide();
 		}
 
 		$(this.productAddElt).click(function(e) {
@@ -28,26 +35,65 @@ class ContractForm {
 		let newProductPrototype = this.productPrototype.replace(/__name__/g, this.productIndex);
 		this.productIndex++;
 
-		let productForm = this.addDeleteLink(newProductPrototype);
-		$(this.productAddElt).before(productForm);
+		let productForm = this.addValidateButton(newProductPrototype);
+		$(this.productContainer).append(productForm);
 		this.customizeSelectBox();
 	}
 
-	addDeleteLink(prototype) {
-		let $deleteLink = $('<a href="#"><span class="btn btn-danger fas fa-times"> Supprimer</span></a>');
-		let prototypeWithDeleteLink = $(prototype).append($deleteLink);
+	addValidateButton(prototype) {
+		let $validateButton = $('<a href="#"><span class="btn btn-primary fas fa-check"> Ajouter</span></a>');
+		let prototypeWithValidateButton = $(prototype).append($validateButton);
+		let prototypeChildrenId = $(prototype).children('div').attr('id');
+		let prototypeIndex = Number(prototypeChildrenId.slice(-1));
 
-		$deleteLink.click(function(e) {
+		let self = this;
+		$validateButton.click(function(e) {
 			e.preventDefault();
-			$(prototypeWithDeleteLink).fadeOut(500, function() {
-				$(this).remove();
+			$(prototypeWithValidateButton).fadeOut(500, function() {
+				self.addTableLine(prototypeIndex , $(this));
 			});
 		});
 
-		return prototypeWithDeleteLink;
+		return prototypeWithValidateButton;
 	}
 
-	customizeSelectBox(){
+	addTableLine(i, prototype) {
+		let tableIndex = i + 1;
+		let name = $('#contract_products_' + i + '_name').val();
+		let $line = $('<tr><th>' + tableIndex + '</th><td>' + name + '</td></tr>');
+
+		let $td = $('<td></td>');
+		let $editLink = $('<a class="mr-2" href="#"><span class="btn btn-primary fas fa-pen"></span></a>');
+		let $deleteLink = $('<a class="mr-2" href="#"><span class="btn btn-danger fas fa-times"></span></a>');
+
+		let actionsLink = $td.append($editLink, $deleteLink);
+		let tableLine = $line.append(actionsLink);
+		$(this.tableBodyElt).append(tableLine);
+
+		$editLink.click(function(e) {
+			e.preventDefault();
+			$(prototype).fadeIn(500);
+		});
+
+		$deleteLink.click(function(e) {
+			e.preventDefault();
+			$(prototype).fadeOut(500, function() {
+				$(this).remove();
+			});
+
+			$line.fadeOut(500, function() {
+				$(this).remove();
+			});
+
+			//if ($(this.tableBodyElt + ' tr').length < 0) {
+			//	$(self.tableElt).fadeOut(500);
+			//}
+		});
+
+		
+	}
+
+	customizeSelectBox() {
 		$(this.selectElt).select2();
 	}
 }
@@ -56,6 +102,9 @@ $(function() {
 	let domElt = {
 		productContainer: '#contract_products',
 		productAddElt: '#add-product',
+		tableElt: 'table',
+		tableBodyElt: 'tbody',
+		tableBodyRowElt: 'tbody tr',
 		selectElt: 'select'
 	}
 	const contractForm = new ContractForm(domElt);
