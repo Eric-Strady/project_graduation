4370a897527d45e725c61930805482e50416e75d
Eric-Strady <strady60@gmail.com>
2019-06-11 09:48:29 +0200

    add fieldtype and help messages

--- src/Form/GrowerType.php
+++ src/Form/GrowerType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Grower;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GrowerType extends AbstractType
@@ -12,13 +13,21 @@ class GrowerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', null, [
+                'help' => 'Le nom renseigné ici servira également de description pour le marqueur de la carte.'
+            ])
             ->add('address')
             ->add('city')
             ->add('postal_code')
-            ->add('gps_lat')
-            ->add('gps_lng')
-            ->add('email')
+            ->add('gps_lat', null, [
+                'help' => 'Exemple de latitude: 48.862725'
+            ])
+            ->add('gps_lng', null, [
+                'help' => 'Exemple de longitude: 2.287592'
+            ])
+            ->add('email', EmailType::class, [
+                'required' => false
+            ])
             ->add('website')
             ->add('phone')
         ;
