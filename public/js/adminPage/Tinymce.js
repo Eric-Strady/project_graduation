9f215eabf0e5749b9313442fa73236b25fb643c5
Eric-Strady <strady60@gmail.com>
2019-06-25 00:30:26 +0200

    remove h1 and h2

--- public/js/adminPage/Tinymce.js
+++ public/js/adminPage/Tinymce.js
@@ -10,7 +10,7 @@ class Tinymce {
     		height: 300,
     		language: 'fr_FR',
 			plugins: 'autosave preview autolink link pagebreak nonbreaking advlist lists textcolor wordcount contextmenu colorpicker help',
-			block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6',
+			block_formats: 'Paragraph=p;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6',
 			toolbar: 'formatselect | bold italic strikethrough forecolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat'
 		});
 	}
